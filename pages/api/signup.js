0905676be1f7278ac36fd18d3db1cc7736c7d2bc
Arun Singh <aruns.ug21.cs@nitp.ac.in>
2023-09-28 11:32:31 +0530

    make all product page

---
+++ pages/api/signup.js
@@ -0,0 +1,5 @@
+const connectToMongo = require("/db");
+const User = require("/models/User");
+// const bcrypt = require("bcryptjs");
+
+connectToMongo();
--- pages/tshirt.js
+++ pages/tshirt.js
@@ -10,6 +10,9 @@ const TShirt = ({ products }) => {
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4 justify-center">
+            {Object.keys(products).length === 0 && (
+              <p>Sorry Currently all Tshirt are out stock</p>
+            )}
             {Object.keys(products).map((item) => {
               return (
                 <Link
